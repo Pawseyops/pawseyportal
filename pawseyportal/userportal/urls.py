ad9d5c1d1cc419eaa61c36268be9bebc148d00b5
ensignexpendable <mark@ensignexpendable.com>
2015-12-07 17:30:30 +0800

    Added View for testing json api for external interface.

---
+++ pawseyportal/userportal/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required, permission_required
+
+from . import views
+
+# List of urls for the announce app. The login required wrappers are stated where we're using class based views.
+urlpatterns = [
+    url(r'^api/listAllocations/$', views.listAllocationsView, name='list_allocations'),
+]
