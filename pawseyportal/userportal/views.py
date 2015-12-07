014a3e4f1dbea06fb2bf75df306480d48c02bfbe
ensignexpendable <mark@ensignexpendable.com>
2015-12-07 17:41:11 +0800

    Missing files from previous commit.

--- pawseyportal/userportal/views.py
+++ pawseyportal/userportal/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# List All Currently Active Allocations
+def listAllocationsView(request):
+    response_data = {}
+    for allocation in Allocation.objects.filter(start__lte=datetime.date.today()).filter(end__gte=datetime.date.today()).exclude(suspend='True'):
+        alloc = {}
+        alloc['name'] = allocation.name
+        alloc['project'] = allocation.project.title
+        alloc['priorityArea'] = allocation.priorityArea.name
+        alloc['serviceunits'] = allocation.serviceunits
+        alloc['service'] = allocation.service.name
+
+        response_data[allocation.id]=alloc
+    return JsonResponse(response_data)
