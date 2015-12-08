c712bc1d5c93485a3e383ee820dbb4e1a5474969
ensignexpendable <mark@ensignexpendable.com>
2015-12-08 14:30:48 +0800

    Adding command line auth system

--- pawseyportal/userportal/views.py
+++ pawseyportal/userportal/views.py
@@ -7,27 +7,36 @@ from django.core.exceptions import PermissionDenied
 
 import datetime
 
+# The API is meant to not require the csrf headers.
+from django.views.decorators.csrf import csrf_exempt
+
 # List All Currently Active Allocations
+@csrf_exempt
 def listAllocationsView(request):
-    #TODO: if authenticated or post variables exist for django.contrib.auth.authenticate authentication (to keep the python client simple for the heavy lifting of Pawsey account activation).
-    
-    # Make sure we are authenticated, or if not see if we can authenticate with POST variables.
+    # Make sure we are authenticated, or if not see if we can authenticate with POST variables. This is to keep from needing cookie management in command line tools.
     user = request.user
     if user is  None:
         #check POST Variables or return
-        raise PermissionDenied
-    else:
-        if user.is_active and user.is_superuser:
-            response_data = {}
-            for allocation in Allocation.objects.filter(start__lte=datetime.date.today()).filter(end__gte=datetime.date.today()).exclude(suspend='True'):
-                alloc = {}
-                alloc['name'] = allocation.name
-                alloc['project'] = allocation.project.title
-                alloc['priorityArea'] = allocation.priorityArea.name
-                alloc['serviceunits'] = allocation.serviceunits
-                alloc['service'] = allocation.service.name
-
-                response_data[allocation.id]=alloc
-            return JsonResponse(response_data)
+        if request.method == 'POST':
+            usernm = request.POST["username"]
+            passwd = request.POST["password"]
+            user = authenticate(username=usernm, password=passwd)
+            if user is None:
+                raise PermissionDenied
         else:
+            raise PermissionDenied
+
+    if user.is_active and user.is_superuser:
+        response_data = {}
+        for allocation in Allocation.objects.filter(start__lte=datetime.date.today()).filter(end__gte=datetime.date.today()).exclude(suspend='True'):
+            alloc = {}
+            alloc['name'] = allocation.name
+            alloc['project'] = allocation.project.title
+            alloc['priorityArea'] = allocation.priorityArea.name
+            alloc['serviceunits'] = allocation.serviceunits
+            alloc['service'] = allocation.service.name
+
+            response_data[allocation.id]=alloc
+        return JsonResponse(response_data)
+    else:
             raise PermissionDenied
