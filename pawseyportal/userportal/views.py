8a838af339c14df0cf5bf88bba1d2ba7e9d6ee1f
ensignexpendable <mark@ensignexpendable.com>
2015-12-08 11:28:22 +0800

    Adding missing execption import

--- pawseyportal/userportal/views.py
+++ pawseyportal/userportal/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
+from django.core.exceptions import PermissionDenied
 
 import datetime
 
