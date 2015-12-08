44164f8da909ddf55d8bcab103c14e36183b59a1
ensignexpendable <mark@ensignexpendable.com>
2015-12-08 16:16:30 +0800

    Adding initial command line portal tools

---
+++ portaltools/listProjects.py
@@ -0,0 +1,47 @@
+#!/bin/python
+import httplib
+import urllib
+import json
+import ConfigParser
+import os
+
+# Preliminary Test Script for framework of command line tools to access Pawsey Portal
+
+def listAllocations(authParams):
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    
+    # Use httplib to request the data
+    conn = httplib.HTTPSConnection(servername)
+    listAllocationsUrl=url + "/portal/api/listAllocations/"
+    conn.request("POST", listAllocationsUrl, authParams, headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    # Unencode and return
+    return json.loads(data)
+
+# Configuration
+config = ConfigParser.RawConfigParser()
+config.read(os.path.expanduser('~/.pawseyportal'))
+
+username = config.get('auth','username')
+password = config.get('auth','password')
+
+servername = config.get('portal','servername')
+url = config.get('portal','url')
+
+# Encode  Credentials for POSTing
+params = urllib.urlencode({'username': username, 'password': password})
+
+allocations = listAllocations(params)
+
+for allId ,allocation in allocations.iteritems():
+    print("Project Name: %s" % (allocation["project"]))
+    print("Project ID: %s" % (allId))
+    print("Allocation name: %s" % (allocation["name"]))
+    print("Pawsey Service: %s" % (allocation["service"]))
+    print("Priority Area: %s" % (allocation["priorityArea"]))
+    print("Service Units: %s" % (allocation["serviceunits"]))
+    print("")
+
+
